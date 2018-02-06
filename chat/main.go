29b5bb0d3a9c96f796dd6519b2774b76c14359be
avalchev94 <asen.valchev1@gmail.com>
2018-02-06 21:53:09 +0200

    Added working version of main.go

--- chat/main.go
+++ chat/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"path/filepath"
 	"sync"
@@ -29,11 +30,15 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	go func() {
-		for {
-		}
-	}()
-	for {
+	r := newRoom()
+	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/room", r)
 
+	// get the room going
+	go r.run()
+
+	// start the web server
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
 	}
 }
