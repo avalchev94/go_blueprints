c0501666cc8c5bda3720b12deee0f62b1976f5f1
avalchev94 <asen.valchev1@gmail.com>
2018-09-07 11:00:35 +0300

    Implemented chapter 9 - AnswerApp. Have finished comments' voting. Created the API endpoint. Understood how Google AppEngine works.

---
+++ answerapp/api/main.go
@@ -0,0 +1,11 @@
+package api
+
+import (
+	"net/http"
+)
+
+func init() {
+	http.HandleFunc("/api/questions/", handleQuestions)
+	http.HandleFunc("/api/answers/", handleAnswers)
+	http.HandleFunc("/api/votes/", handleVotes)
+}
